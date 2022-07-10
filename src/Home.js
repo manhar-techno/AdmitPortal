447cc2d07998ba24658626868ab707c2657e516c
Manhar Joshi <ishanjsh2@gmail.com>
2022-07-10 20:14:28 +0530

    first from mac

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+export default function Home() {
+  return (
+    <div className="home-background">
+      <h1>Welcome,</h1>
+      <h4 className="heading-follow">Kindly refer to the following</h4>
+      <div className="buttons">
+        <button className="button-home">Check Data</button>
+        <button className="button-home">Add new person</button>
+      </div>
+    </div>
+  );
+}
